996882f0bba5c6cb18e8b05bbd7d99b18eedfb60
Adrian Nastał <81535800+Bazky@users.noreply.github.com>
2023-04-17 19:36:37 +0200

    2

---
+++ src/js/02-timer.js
@@ -0,0 +1,70 @@
+import flatpickr from 'flatpickr';
+import 'flatpickr/dist/flatpickr.min.css';
+
+const dateInput = document.getElementById('datetime-picker');
+const startBtn = document.querySelector('button[data-start]');
+const daysValue = document.querySelector('span[data-days]');
+const hoursValue = document.querySelector('span[data-hours]');
+const minutesValue = document.querySelector('span[data-minutes]');
+const secondsValue = document.querySelector('span[data-seconds]');
+
+const options = {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+    if (selectedDates[0] < new Date()) {
+      window.alert('Please choose a date in the future');
+    } else {
+      startBtn.removeAttribute('disabled');
+    }
+  },
+};
+
+flatpickr(dateInput, options);
+
+let countdownInterval;
+
+function startCountdown() {
+  const countdownDate = flatpickr.parseDate(dateInput.value);
+
+  function updateCountdown() {
+    const msLeft = countdownDate - new Date();
+    if (msLeft <= 0) {
+      clearInterval(countdownInterval);
+      daysValue.innerText = '00';
+      hoursValue.innerText = '00';
+      minutesValue.innerText = '00';
+      secondsValue.innerText = '00';
+      startBtn.setAttribute('disabled', '');
+      return;
+    }
+  }
+  countdownInterval = setInterval(updateCountdown, 1000);
+}
+
+startBtn.addEventListener('click', startCountdown);
+
+function convertMs(ms) {
+  // Number of milliseconds per unit of time
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  // Remaining days
+  const days = Math.floor(ms / day);
+  // Remaining hours
+  const hours = Math.floor((ms % day) / hour);
+  // Remaining minutes
+  const minutes = Math.floor(((ms % day) % hour) / minute);
+  // Remaining seconds
+  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+
+  return { days, hours, minutes, seconds };
+}
+
+console.log(convertMs(2000)); // {days: 0, hours: 0, minutes: 0, seconds: 2}
+console.log(convertMs(140000)); // {days: 0, hours: 0, minutes: 2, seconds: 20}
+console.log(convertMs(24140000)); // {days: 0, hours: 6 minutes: 42, seconds: 20}
